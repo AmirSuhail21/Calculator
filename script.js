464b5e8bbe13a7de70e825dcde0fa148280f4046
Amir Suhail 모 <mannuraien3@gmail.com>
2026-07-05 10:24:32 +0530

    Improve calculator input handling and result display
    Refactor calculator logic to handle input and results more robustly.

--- script.js
+++ script.js
@@ -2,49 +2,53 @@ let operatorButtons = document.querySelectorAll(".operator");
 let digitButtons = document.querySelectorAll(".digit");
 let ac = document.querySelector(".ac");
 let enter = document.querySelector("#enter");
+let input = document.querySelector("#input");
+let resultDiv = document.querySelector("#result");
 
-digitButtons.forEach( (btn) =>{
-    btn.addEventListener("click" , () =>{
-        input.value += btn.innerText;
-        console.log("print your digit!")
-    }
-    )
-}
-)
 let num = null;
 let operator = null;
-operatorButtons.forEach((btn) =>{
-    btn.addEventListener("click" ,() =>{
-        num = parseFloat(input.value)
-        operator = btn.innerText;
-        input.value = "";
-    } )
+
+digitButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        input.value += btn.innerText;
+    });
 });
 
-
-enter.addEventListener("click" , ()=>{
-    let num2 = parseFloat(input.value);
-    let result;
-
-    if(operator === "+"){
-        result = num + num2;
-    }
-    else if(operator === "−"){
-        result = num - num2;
-    }
-    else if(operator === "×"){
-        result = num * num2;
-    }
-    else if(operator === "÷"){
-        result = num2 !== 0 ? num / num2 : Error;
-    }   
-        let res= document.querySelector("#result").innerText = "Result : " + result;
-        input.value = "";
-
+operatorButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (input.value !== "") {
+            num = parseFloat(input.value);
+            operator = btn.innerText;
+            input.value = "";
+        }
+    });
 });
 
-ac.addEventListener("click", ()=>{
+enter.addEventListener("click", () => {
+    if (num !== null && operator !== null && input.value !== "") {
+        let num2 = parseFloat(input.value);
+        let result;
+
+        if (operator === "+") {
+            result = num + num2;
+        } else if (operator === "−") {
+            result = num - num2;
+        } else if (operator === "×") {
+            result = num * num2;
+        } else if (operator === "÷") {
+            result = num2 !== 0 ? num / num2 : "Error";
+        }
+
+        resultDiv.innerText = "Result : " + result;
+        input.value = "";
+        num = null;
+        operator = null;
+    }
+});
+
+ac.addEventListener("click", () => {
     input.value = "";
-    result.innerText = "Result :";
-})
-
+    resultDiv.innerText = "Result :";
+    num = null;
+    operator = null;
+});
